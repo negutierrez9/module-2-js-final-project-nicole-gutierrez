c5d27a00cddc43274e09238e7b1d2044db0d3a13
Nicole Gutierrez <nicole.gutierrez.bvt2023@gmail.com>
2024-02-21 20:02:17 -0800

    drop down menu, search results

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const searchForm = document.querySelector('form'); 
 const searchBarDiv = document.querySelector('.search-result'); 
-const container = document.getElementsByClassName('container')
+const container = document.querySelector('.container')
 let searchInput = ''; 
-const APP_KEY = 'da70d5e161c815b64e8efbb9be7ddf04'
+let dropdown = document.querySelector('.dropdown')
 
 const options = {
     method: 'GET',
@@ -19,7 +19,7 @@ searchForm.addEventListener('submit', (e) => {
 }); 
 
 async function fetchAPI () {
-    const baseURL = 'https://api.themoviedb.org/3/search/multi?query=solo%20leveling&include_adult=false&language=en-US&page=1'; 
+    const baseURL = `https://api.themoviedb.org/3/search/multi?query=${searchInput}&include_adult=false&language=en-US&page=1`; 
     const response = await fetch(baseURL, options); 
     const data = await response.json(); 
     generateHTML(data.results);
@@ -27,31 +27,44 @@ async function fetchAPI () {
 }
 
 function generateHTML(results) {
+    container.classList.remove('initial'); 
     let generatedHTML = ''; 
     const imgURL = "http://image.tmdb.org/t/p/original/"
     results.map(result => {
         generatedHTML +=
         `
         <div class="item">
-            <img src="${imgURL + result.poster_path}" alt="solo leveling poster">
-            <div class="flex-container">
-                <h1 class="title">${result.name}</h1>
-                <a class="view-button" href="#">View Movie</a>
+            <img src="${!result.poster_path ? "./Sad photo icon.jpeg" : imgURL + result.poster_path}" alt="poster">
+            <div class="top-flex-container">
+                <h1 class="title">${!result.title ? result.name : result.title}</h1>
+                <a class="liked-movies" href="#"><ion-icon name="heart-outline"></ion-icon></a>
             </div>
-            <p class="item-data">
-                Rating: 
-                <button class="rating-button">
+            <div class="bottom-flex-container">
+                <p class="item-data">Rating: </p>
+                <a href="#" class="rating-button">
                     <ion-icon name="star-outline"></ion-icon>
+                </a>
+                <a href="#" class="rating-button">
                     <ion-icon name="star-outline"></ion-icon>
+                </a>
+                <a href="#" class="rating-button">
                     <ion-icon name="star-outline"></ion-icon>
+                </a>
+                <a href="#" class="rating-button">
                     <ion-icon name="star-outline"></ion-icon>
+                </a>
+                <a href="#" class="rating-button">
                     <ion-icon name="star-outline"></ion-icon>
-                </button>
-            </p>
-            <p class="item-data">Date: ${result.first_air_date}</p>
+                </a>
+            </div>
+            <p class="item-data">Date: ${!result.first_air_date ? result.release_date : result.first_air_date}</p>
         </div>
         `
     }); 
     searchBarDiv.innerHTML = generatedHTML; 
 }
+
+dropdown.addEventListener('click', (e) => {
+    dropdown.classList.contains('closed') ? dropdown.classList.remove('closed') : dropdown.classList.add('closed'); 
+})
 
